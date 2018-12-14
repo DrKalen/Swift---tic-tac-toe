677b0dc00a972aaf90e713aad73740d0be34d09f
Kalen Hammann <kalen7@gmail.com>
2018-12-14 14:23:53 -0500

    Winner identified and game stopped for rows win

--- tictactoe2/ViewController.swift
+++ tictactoe2/ViewController.swift
@@ -12,13 +12,15 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var whoseTurnLabel: UILabel!
     
+    var activeGame = true
     var activePlayer = 1
-    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]         // 0 = empty, 1 = nought, 2 = cross
+    var winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     
     @IBAction func button(_ sender: UIButton) {
         let activePosition = sender.tag - 1
         
-        if gameState[activePosition] == 0 {
+        if gameState[activePosition] == 0 && activeGame {
             gameState[activePosition] = activePlayer
             if activePlayer == 1 {
                 sender.setImage(UIImage(named: "nought.png"), for: [])
@@ -27,10 +29,18 @@ class ViewController: UIViewController {
                 sender.setImage(UIImage(named: "cross.png"), for: [])
                 activePlayer = 1
             }
+            
+            for combination in winningCombinations {
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                    // we have a winner!
+                    activeGame = false
+                    print(gameState[combination[0]])   //  the identity of the winner
+                }
+            }
         }
         
         
-        print(sender.tag)
+//        print(sender.tag)
     }
     
     
