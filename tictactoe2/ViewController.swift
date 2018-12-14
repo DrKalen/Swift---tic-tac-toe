18f8f298e79cf615867ed0c10d9e0a8d3167a7ae
Kalen Hammann <kalen7@gmail.com>
2018-12-14 11:49:56 -0500

    initial commit including UI

---
+++ tictactoe2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tictactoe2
+//
+//  Created by Kalen Hammann on 12/13/18.
+//  Copyright © 2018 Human Family. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var whoseTurnLabel: UILabel!
+    
+    @IBAction func button(_ sender: Any) {
+        print("Button Pressed")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
